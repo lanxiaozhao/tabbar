69b8010d2b4c224a2e97faf842b469bd9c8d08e9
zhaopan <1406225321@qq.com>
2020-03-07 16:18:57 +0800

    配置修改

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+const path = require('path');
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('assets', resolve('src/assets'))
+      .set('components', resolve('src/components'))
+      
+  }
+}
